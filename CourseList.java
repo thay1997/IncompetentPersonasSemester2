b8896753b806cf0bf2600a0709aca3559ad8ce3b
thay1997 <49237779+thay1997@users.noreply.github.com>
2021-05-18 17:16:05 +0100

    Add files via upload

--- CourseList.java
+++ CourseList.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 //Implements the List interface that means we can make a generic array list containing this object
 public class CourseList implements List{
@@ -8,17 +9,28 @@ public class CourseList implements List{
 	
 	//Constructor uses the course array created by the file parser to make courses and add them to the LoC
 	public CourseList(ArrayList<String> coursesFromFile) {
-		for(int i = 0; i< coursesFromFile.size(); i++) {
-			String line = coursesFromFile.get(i);
-			String[] tokens = line.split(",");
-			String name = tokens[0]; //First element in the array is always the course name 
-			int staffNum = Integer.parseInt(tokens[1]); //Second element in the array is always the staff number
+		for(String string: coursesFromFile) {
+			String line = string;
+			Scanner s = new Scanner(line);
+			String name = s.next();
+			int staffNum = Integer.parseInt(s.next());
 			ArrayList<String> trainingReq = new ArrayList<String>();
-			for(int j = 2; j < tokens.length;j++ ) { //The remaining elements are all Training required
-				trainingReq.add(tokens[j]);
-			}
+			while(s.hasNext())
+				trainingReq.add(s.next());
+
+//        	******Another way to create courses from the file, requires changing FileHandler slightly******
+//			for(int i = 0; i< coursesFromFile.size(); i++) {
+//			String line = coursesFromFile.get(i);
+//			String[] tokens = line.split(",");
+//			String name = tokens[0]; //First element in the array is always the course name 
+//			int staffNum = Integer.parseInt(tokens[1]); //Second element in the array is always the staff number
+//			ArrayList<String> trainingReq = new ArrayList<String>();
+//			for(int j = 2; j < tokens.length;j++ ) { //The remaining elements are all Training required
+//				trainingReq.add(tokens[j]);
+//			}
 			Course c = new Course(name, staffNum, trainingReq);
 			LoC.add(c);
+			s.close();
 		}
 	}
 	
@@ -53,5 +65,14 @@ public class CourseList implements List{
 			line += i.next();
 		
 		return line;
+	}
+	public static void main(String[] args) {
+        ArrayList<String>parsedCourses = new ArrayList<String>();
+        parsedCourses.add("Programming 3 english,maths");
+        parsedCourses.add("Database 2 english,maths");
+        parsedCourses.add("Software_Engineering 1 maths");
+        
+        CourseList c = new CourseList(parsedCourses);
+        System.out.println(c);
 	}
 }
--- TeacherList.java
+++ TeacherList.java
@@ -1,6 +1,9 @@
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
+
+import org.json.JSONArray;
 
 //Implements the List interface that means we can make a generic array list containing this object
 public class TeacherList implements List {
@@ -9,18 +12,27 @@ public class TeacherList implements List {
 	
 	//Constructor uses the teacher array created by file parser to make Teachers and add them to the LoT
 	public TeacherList(ArrayList<String> teachersFromFile) {
-		
+
 		//Takes the String array list and make teacher object for each element and adds it to the LoT
-		for(int i = 0; i<teachersFromFile.size();i++) {
-        	String line = teachersFromFile.get(i);
-        	String[] tokens = line.split(",");
-        	String name = tokens[0]; //First element in the array is always the teachers name
+		for(String input :teachersFromFile) {
+        	String line = input;
+        	Scanner s = new Scanner(line);
         	ArrayList<String> training = new ArrayList<String>();
-        	for(int j = 1; j< tokens.length;j++) { //The remaining elements are all training 
-        		training.add(tokens[j]);
-        	}
+        	String name = s.next();
+        	while(s.hasNext())
+        		training.add(s.next());
+
+//        	******Another way to create teachers from the file, requires changing FileHandler slightly******
+//    		for(int i = 0; i<teachersFromFile.size();i++) {
+//        	String[] tokens = line.split(",");
+//        	String name = tokens[0]; //First element in the array is always the teachers name
+//        	ArrayList<String> training = new ArrayList<String>();
+//        	for(int j = 1; j< tokens.length;j++) { //The remaining elements are all training 
+//        		training.add(tokens[j]);
+//        	}
         	Teacher t = new Teacher(name,training);
         	LoT.add(t);
+        	s.close();
         
         }
 	}
@@ -60,6 +72,20 @@ public class TeacherList implements List {
 			line += "\n" + i.next();
 		
 		return line;
+	}
+	
+	public static void main(String[] args) {
+		FileHandler fileHandler = new FileHandler();
+        // Read the file
+        JSONArray array = fileHandler.readFile();
+
+        // Parse the teachers
+        ArrayList<String> parsedTeachers = fileHandler.parseTeachers(array);
+        
+        TeacherList teachers = new TeacherList(parsedTeachers);
+        
+        
+        System.out.println(teachers);
 	}
 
 }
